1ba95b875485521cb2e92d74327212e9061c7e30
Andy Albrecht <aalbrecht33733@gmail.com>
2022-03-28 12:03:14 -0400

    Identified as 'FarmStand' project, added 'useUnifiedTopology: true'

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 
 const Product = require('./models/product');
 
-mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("======================================")
         console.log("MONGO Connction OPEN!!")
